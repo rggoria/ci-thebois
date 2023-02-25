4ba9e7f6051a563c9590ef5e13270032dded1a64
Ram Emerson S. Goria <goria.reg@gmail.com>
2023-02-26 02:12:30 +0800

    Added small details in the Admin Module

--- application/views/admin/admin_homepage.php
+++ application/views/admin/admin_homepage.php
@@ -1,109 +1,147 @@
+<!-- Hero Section -->
 <section class="bg-dark py-5 text-center text-white container-fluid">
     <h1>Welcome to Gundam, Admin:</h1>
     <h5>You Can Find All The Grades You Need For Your Dream Gundam Model</h5>
 </section>
 
-<div class="container mt-3 mb-3">
-    <div class="row">
-        <div class="col-4">
-            <div class="card">
-                <!-- <img src="https://i.pinimg.com/originals/7f/88/69/7f88697aa9e6adc23f662ff5331f00e9.jpg" class="card-img-top" alt="..."> -->
-                <div class="card-body">
-                    <h5 class="card-title">HG (High Grade)</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-white border-dark">View more</a>
+<!-- Dashboard Section -->
+<section>
+    <div class="container my-3">
+        <h2>Dashboard</h2>
+        <div class="row">
+            <div class="col-md-3 my-2">
+                <div class="card text-center">
+                    <div class="card-body">
+                        <img src="<?= base_url('resources/images/group.png') ?>" class="icon" alt="User icon" height="50">
+                        <h5 class="card-title">Total Users</h5>
+                        <p class="card-text">1000</p>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col-4">
-            <div class="card">
-                <!-- <img src="https://static.vecteezy.com/system/resources/thumbnails/005/607/208/small_2x/meat-and-meat-products-color-set-simple-design-on-brown-background-flat-style-cartoon-illustration-free-vector.jpg" class="card-img-top" alt="..."> -->
-                <div class="card-body">
-                    <h5 class="card-title">Gundam Case #2</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-white border-dark">View more</a>
+            <div class="col-md-3 my-2">
+                <div class="card text-center">
+                    <div class="card-body">
+                        <img src="<?= base_url('resources/images/inventory.png') ?>" class="icon" alt="User icon" height="50">
+                        <h5 class="card-title">Total Inventory</h5>
+                        <p class="card-text">1000</p>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col-4">
-            <div class="card">
-                <!-- <img src="https://img.freepik.com/free-vector/drinks_1308-4653.jpg?w=2000" class="card-img-top" alt="..."> -->
-                <div class="card-body">
-                    <h5 class="card-title">Gundam Case #3</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-white border-dark">View more</a>
+            <div class="col-md-3 my-2">
+                <div class="card text-center">
+                    <div class="card-body">
+                        <img src="<?= base_url('resources/images/time-management.png') ?>" class="icon" alt="User icon" height="50">
+                        <h5 class="card-title">Total Transaction</h5>
+                        <p class="card-text">1000</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 my-2">
+                <div class="card text-center">
+                    <div class="card-body">
+                        <img src="<?= base_url('resources/images/drop-shipping.png') ?>" class="icon" alt="User icon" height="50">
+                        <h5 class="card-title">Total Dropship</h5>
+                        <p class="card-text">1000</p>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+</section>
 
-
-<section class="py-5 container">
-    <div class="card">
-        <ul class="nav nav-tabs nav-fill mb-3" id="myTab" role="tablist">
-            <li class="nav-item" role="presentation">
-                <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true"><i class="fas fa-user-circle fa-fw me-2"></i>Users</button>
-            </li>
-            <li class="nav-item" role="presentation">
-                <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false"><i class="fas fas fa-box fa-fw me-2"></i>Inventory</button>
-            </li>
-            <li class="nav-item" role="presentation">
-                <button class="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact" type="button" role="tab" aria-controls="contact" aria-selected="false"><i class="fas fa-chart-line fa-fw me-2"></i>Transaction</button>
-            </li>
-        </ul>
-        <div class="tab-content" id="myTabContent">
-            <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-                <table class="table table-hover">
-                    <caption class="text-center">end of users</caption>
-                    <thead>
-                        <tr>
-                        <th scope="col">Name</th>
-                        <th scope="col">Email</th>
-                        <th scope="col">Status</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                        <td><a href="#">John Doe</a></td>
-                        <td><a href="mailto:john.doe@example.com">john.doe@example.com</a></td>
-                        <td><a href="tel:+1234567890">User</a></td>
-                        </tr>
-                        <tr>
-                        <td><a href="#">Jane Doe</a></td>
-                        <td><a href="mailto:jane.doe@example.com">jane.doe@example.com</a></td>
-                        <td><a href="tel:+1234567890">Admin</a></td>
-                        </tr>
-                    </tbody>
-                </table>
+<!-- Table Section -->
+<section>
+    <div class="container mb-5">
+        <h2>Table of Contents</h2>
+        <div class="card">
+            <ul class="nav nav-tabs nav-fill mb-3" id="myTab" role="tablist">
+                <li class="nav-item" role="presentation">
+                    <button
+                        class="nav-link active" id="user-tab" data-bs-toggle="tab" data-bs-target="#user" type="button" role="tab" aria-controls="user" aria-selected="false">Users
+                    </button>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <button
+                        class="nav-link" id="inventory-tab" data-bs-toggle="tab" data-bs-target="#inventory" type="button" role="tab" aria-controls="inventory" aria-selected="false">Inventory
+                    </button>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <button
+                        class="nav-link" id="transaction-tab" data-bs-toggle="tab" data-bs-target="#transaction" type="button" role="tab" aria-controls="transaction" aria-selected="false">Transaction
+                    </button>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <button
+                        class="nav-link" id="dropship-tab" data-bs-toggle="tab" data-bs-target="#dropship" type="button" role="tab" aria-controls="dropship" aria-selected="false">Dropship
+                    </button>
+                </li>
+            </ul>
+            <div class="tab-content" id="myTabContent">
+                <div class="tab-pane fade show active" id="user" role="tabpanel" aria-labelledby="user-tab">
+                    <table class="table table-hover">
+                        <div class="ms-2 d-flex align-items-center">                                                        
+                            <img src="<?= base_url('resources/images/group.png') ?>" alt="" height="25">
+                            <span class="ms-2">List of Users</span>
+                            <div class="ms-auto">
+                                <button>Add</button>
+                                <button>2</button>
+                            </div>
+                        </div>
+                        <caption class="text-center">end of users</caption>
+                        <thead>
+                            <tr>
+                                <th scope="col">Name</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                            <td><a href="#">John Doe</a></td>
+                            <td><a href="mailto:john.doe@example.com">john.doe@example.com</a></td>
+                            <td><a href="tel:+1234567890">User</a></td>
+                            </tr>
+                            <tr>
+                            <td><a href="#">Jane Doe</a></td>
+                            <td><a href="mailto:jane.doe@example.com">jane.doe@example.com</a></td>
+                            <td><a href="tel:+1234567890">Admin</a></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="tab-pane fade" id="inventory" role="tabpanel" aria-labelledby="inventory-tab">
+                    <table class="table table-hover">
+                        <caption class="text-center">end of users</caption>
+                        <thead>
+                            <tr>
+                            <th scope="col">Name</th>
+                            <th scope="col">Status</th>
+                            <th scope="col">Price</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                            <td><a href="#">John Doe</a></td>
+                            <td><a href="mailto:john.doe@example.com">john.doe@example.com</a></td>
+                            <td><a href="tel:+1234567890">User</a></td>
+                            </tr>
+                            <tr>
+                            <td><a href="#">Jane Doe</a></td>
+                            <td><a href="mailto:jane.doe@example.com">jane.doe@example.com</a></td>
+                            <td><a href="tel:+1234567890">Admin</a></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="tab-pane fade" id="transaction" role="tabpanel" aria-labelledby="transaction-tab">
+                    ...
+                </div>
+                <div class="tab-pane fade" id="dropship" role="tabpanel" aria-labelledby="dropship-tab">
+                    ...
+                </div>
             </div>
-            <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-                <table class="table table-hover">
-                    <caption class="text-center">end of users</caption>
-                    <thead>
-                        <tr>
-                        <th scope="col">Name</th>
-                        <th scope="col">Status</th>
-                        <th scope="col">Price</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                        <td><a href="#">John Doe</a></td>
-                        <td><a href="mailto:john.doe@example.com">john.doe@example.com</a></td>
-                        <td><a href="tel:+1234567890">User</a></td>
-                        </tr>
-                        <tr>
-                        <td><a href="#">Jane Doe</a></td>
-                        <td><a href="mailto:jane.doe@example.com">jane.doe@example.com</a></td>
-                        <td><a href="tel:+1234567890">Admin</a></td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-            <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
         </div>
-    </div>    
+    </div>
 </section>
 
 
