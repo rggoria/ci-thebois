2aca88618db97b9185970cb1fe6f60ac2ad7be52
OverZenith <phc.christian0730@outlook.ph>
2023-03-13 16:24:03 +0800

    Major Changes: - Revamped UI - Added Registration Functionality - Added Cart

--- application/controllers/Store.php
+++ application/controllers/Store.php
@@ -4,14 +4,34 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Store extends CI_Controller {
     public function __construct() {
         parent::__construct();
-        $this->load->helper(['url']);
+        $this->load->helper(['url', 'form']);
     }
 
     public function index() {
+        $this->load->view('include/store/header');
+        $this->load->view('include/store/navbar');
         $this->load->view('store/store_index');
+        $this->load->view('include/store/footer');
     }
 
-    public function product() {
-        $this->load->view('store/store_products');
+    public function catalog($category) {
+        $this->load->view('include/store/header');
+        $this->load->view('include/store/navbar');
+        
+        if ($category == "high_grade") {
+            $this->load->view('store/store_catalog_highgrade');
+        } else if ($category == "real_grade") {
+            $this->load->view('store/store_catalog_realgrade');
+        } else if ($category == "master_grade") {
+            $this->load->view('store/store_catalog_mastergrade');
+        } else if ($category == "perfect_grade") {
+            $this->load->view('store/store_catalog_perfectgrade');
+        } else if ($category == "entry_grade") {
+            $this->load->view('store/store_catalog_entrygrade');
+        } else if ($category == "super_deformed") {
+            $this->load->view('store/store_catalog_superdeformed');
+        }
+        
+        $this->load->view('include/store/footer');
     }
 }
---
+++ application/views/include/store/navbar.php
@@ -0,0 +1,114 @@
+<nav class="navbar navbar-expand-md navbar-light bg-light">
+    <a href="#" class="navbar-brand">Navbrand</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a href="<?= site_url('Store'); ?>" class="nav-link">Home</a>
+            </li>
+            <li class="nav-item">
+                <a href="<?= site_url('Store/catalog/high_grade') ?>" class="nav-link">Shop</a>
+            </li>
+            <li class="nav-item dropdown">
+                <div class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown">
+                    <span><i class="fa-solid fa-user"></i></span> Login
+                </div>
+                <div class="dropdown-menu dropdown-menu-lg-end p-3" style="min-width: 20rem;">
+                    <div class="mb-3">
+                        <label class="form-label">Email</label>
+                        <input type="text" name="" id="" class="form-control" placeholder="example@email.com">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Password</label>
+                        <input type="password" name="" id="" class="form-control" placeholder="your password">
+                    </div>
+
+                    <button type="submit" class="btn btn-primary w-100 mb-2">Login</button> <br>
+                    <p class="text-center">Don't have an account? <span role="button" data-bs-toggle="modal" data-bs-target="#staticModal" class="text-primary text-decoration-underline">Sign-up for free!</span></p>
+                </div>
+            </li>
+            <li class="nav-item dropdown">
+                <div class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown">
+                    <span><i class="fa-solid fa-cart-shopping"></i></span> My Cart
+                </div>
+                <div class="dropdown-menu dropdown-menu-end">
+                    <table class="table dropdown-item">
+                        <thead>
+                            <tr>
+                                <th>Item No.</th>
+                                <th>Name</th>
+                                <th>Qty</th>
+                                <th>Price</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Barbatos</td>
+                                <td>5</td>
+                                <td>₱0</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <div><hr class="dropdown-divider"></div>
+                    <div class="dropdown-item">
+                        <table class="table">
+                            <tr>
+                                <td>Total:</td>
+                                <td>₱0</td>
+                                <td><button class="btn btn-primary">Proceed to Checkout</button></td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+                
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<!-- Registration Modal -->
+<div class="modal fade" id="staticModal" data-bs-backdrop="static" data-bs-keyboard="false">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Sign Up Form</h5>
+                    <button:button class="btn-close" data-bs-dismiss="modal"></button:button>
+                </div>
+
+                <div class="modal-body">
+                    <?= form_open('Users/register_user'); ?>
+                    <div class="mb-3">
+                        <label class="form-label">First Name</label>
+                        <input type="text" name="fname" id="" class="form-control">
+                        <label class="form-label">Last Name</label>
+                        <input type="text" name="lname" id="" class="form-control">
+                    </div>
+
+                    <div class="mb-3">
+                        <label class="form-label">Complete Address</label>
+                        <input type="text" name="caddress" id="" class="form-control">
+                        <label class="form-label">Billing Address</label>
+                        <input type="text" name="baddress" id="" class="form-control">
+                    </div>
+
+                    <div class="mb-3">
+                        <label class="form-label">Contact Number</label>
+                        <input type="text" name="cnumber" id="" class="form-control">
+                        <label class="form-label">Email Address</label>
+                        <input type="email" name="email" id="" class="form-control">
+                        <label class="form-label">Password</label>
+                        <input type="password" name="password" id="" class="form-control">
+                    </div>
+
+                    <button type="submit" class="btn btn-primary w-100">Register</button>
+                    <?= form_close(); ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
