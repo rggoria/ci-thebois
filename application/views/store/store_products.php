bebf1e62f941489d1681a9c3ee03b91be158d109
OverZenith <phc.christian0730@outlook.ph>
2023-03-13 09:47:01 +0800

    Added products page (Not yet connected)

---
+++ application/views/store/store_products.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+   <link rel="stylesheet" href="<?= base_url('resources/css/style_products.css') ?>">
+   <script src="<?= base_url('resources/javascript/script_products.js') ?>" defer></script>
+
+</head>
+<body>
+   
+<div class="container">
+
+   <h3 class="title"> Products </h3>
+
+   <div class="products-container">
+
+      <div class="product" data-name="p-1">
+         <img src="https://www.hlj.com/productthumbs/ban/bans63358_0_1660008038.jpg?v=1660008038" alt="">
+         <h3>G-Gundam</h3>
+         <div class="price">$2.00</div>
+      </div>
+
+      <div class="product" data-name="p-2">
+         <img src="https://www.hlj.com/productthumbs/ban/bann30363_0_1533790566.jpg?v=1533790566" alt="">
+         <h3>Sazabi</h3>
+         <div class="price">$2.00</div>
+      </div>
+
+      <div class="product" data-name="p-3">
+         <img src="https://www.hlj.com/productthumbs/ban/bans61915_0_1631609416.jpg?v=1631609416" alt="">
+         <h3>Hi-Nu Gundam</h3>
+         <div class="price">$2.00</div>
+      </div>
+
+      <div class="product" data-name="p-4">
+         <img src="https://www.hlj.com/productthumbs/ban/bans61661_0_1623924019.jpg?v=1623924019" alt="">
+         <h3>Wing Gundam</h3>
+         <div class="price">$2.00</div>
+      </div>
+
+      <div class="product" data-name="p-5">
+         <img src="https://www.hlj.com/productthumbs/ban/bans57617_0_1558594635.jpg?v=1558594635" alt="">
+         <h3>Crossbone Gundam</h3>
+         <div class="price">$2.00</div>
+      </div>
+
+      <div class="product" data-name="p-6">
+         <img src="https://www.hlj.com/productthumbs/ban/bans57842_0_1565257016.jpg?v=1565257016" alt="">
+         <h3>V-Gundam</h3>
+         <div class="price">$2.00</div>
+      </div>
+
+   </div>
+
+</div>
+
+<div class="products-preview">
+
+   <div class="preview" data-target="p-1">
+      <i class="fas fa-times"></i>
+      <img src="https://www.hlj.com/productthumbs/ban/bans63358_0_1660008038.jpg?v=1660008038" alt="">
+      <h3>G-Gundam</h3>
+      <div class="stars">
+         <i class="fas fa-star"></i>
+         <i class="fas fa-star"></i>
+         <i class="fas fa-star"></i>
+         <i class="fas fa-star"></i>
+         <i class="fas fa-star-half-alt"></i>
+         <span>( 250 )</span>
+      </div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, dolorem.</p>
+      <div class="price">$2.00</div>
+      <div class="buttons">
+         <a href="#" class="buy">buy now</a>
+         <a href="#" class="cart">add to cart</a>
+      </div>
+   </div>
+
+   <div class="preview" data-target="p-2">
+      <i class="fas fa-times"></i>
+      <img src="https://www.hlj.com/productthumbs/ban/bann30363_0_1533790566.jpg?v=1533790566" alt="">
+      <h3>Sazabi</h3>
+      <div class="stars">
+         <i class="fas fa-star"></i>
+         <i class="fas fa-star"></i>
+         <i class="fas fa-star"></i>
+         <i class="fas fa-star"></i>
+         <i class="fas fa-star-half-alt"></i>
+         <span>( 250 )</span>
+      </div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, dolorem.</p>
+      <div class="price">$2.00</div>
+      <div class="buttons">
+         <a href="#" class="buy">buy now</a>
+         <a href="#" class="cart">add to cart</a>
+      </div>
+   </div>
+
+   <div class="preview" data-target="p-3">
+      <i class="fas fa-times"></i>
+      <img src="https://www.hlj.com/productthumbs/ban/bans61915_0_1631609416.jpg?v=1631609416" alt="">
+      <h3>Hi-Nu Gundam</h3>
+      <div class="stars">
+         <i class="fas fa-star"></i>
+         <i class="fas fa-star"></i>
+         <i class="fas fa-star"></i>
+         <i class="fas fa-star"></i>
+         <i class="fas fa-star-half-alt"></i>
+         <span>( 250 )</span>
+      </div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, dolorem.</p>
+      <div class="price">$2.00</div>
+      <div class="buttons">
+         <a href="#" class="buy">buy now</a>
+         <a href="#" class="cart">add to cart</a>
+      </div>
+   </div>
+
+   <div class="preview" data-target="p-4">
+      <i class="fas fa-times"></i>
+      <img src="https://www.hlj.com/productthumbs/ban/bans61661_0_1623924019.jpg?v=1623924019" alt="">
+      <h3>Wing Gundam</h3>
+      <div class="stars">
+         <i class="fas fa-star"></i>
+         <i class="fas fa-star"></i>
+         <i class="fas fa-star"></i>
+         <i class="fas fa-star"></i>
+         <i class="fas fa-star-half-alt"></i>
+         <span>( 250 )</span>
+      </div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, dolorem.</p>
+      <div class="price">$2.00</div>
+      <div class="buttons">
+         <a href="#" class="buy">buy now</a>
+         <a href="#" class="cart">add to cart</a>
+      </div>
+   </div>
+
+   <div class="preview" data-target="p-5">
+      <i class="fas fa-times"></i>
+      <img src="https://www.hlj.com/productthumbs/ban/bans57617_0_1558594635.jpg?v=1558594635" alt="">
+      <h3>Crossbone Gundam</h3>
+      <div class="stars">
+         <i class="fas fa-star"></i>
+         <i class="fas fa-star"></i>
+         <i class="fas fa-star"></i>
+         <i class="fas fa-star"></i>
+         <i class="fas fa-star-half-alt"></i>
+         <span>( 250 )</span>
+      </div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, dolorem.</p>
+      <div class="price">$2.00</div>
+      <div class="buttons">
+         <a href="#" class="buy">buy now</a>
+         <a href="#" class="cart">add to cart</a>
+      </div>
+   </div>
+
+   <div class="preview" data-target="p-6">
+      <i class="fas fa-times"></i>
+      <img src="https://www.hlj.com/productthumbs/ban/bans57842_0_1565257016.jpg?v=1565257016" alt="">
+      <h3>V-Gundam</h3>
+      <div class="stars">
+         <i class="fas fa-star"></i>
+         <i class="fas fa-star"></i>
+         <i class="fas fa-star"></i>
+         <i class="fas fa-star"></i>
+         <i class="fas fa-star-half-alt"></i>
+         <span>( 250 )</span>
+      </div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, dolorem.</p>
+      <div class="price">$2.00</div>
+      <div class="buttons">
+         <a href="#" class="buy">buy now</a>
+         <a href="#" class="cart">add to cart</a>
+      </div>
+   </div>
+
+</div>
+
+</body>
+</html>
